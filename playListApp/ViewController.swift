62b6f7028748ed1795f5ab0af7ce055ec02d6b5c
Angel Chou <cat77120377@hotmail.com>
2019-04-02 10:40:21 +0800

    Initial Commit

---
+++ playListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playListApp
+//
+//  Created by Shun-Ching, Chou on 2019/4/2.
+//  Copyright © 2019 Shun-Ching, Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
